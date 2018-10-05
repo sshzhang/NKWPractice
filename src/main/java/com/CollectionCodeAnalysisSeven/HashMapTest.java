3a0cae478c9fcbe99f9afdb225998aae0e11326c
zxj <1564752861@qq.com>
2018-10-05 21:33:39 +0800

    20181005

--- src/main/java/com/CollectionCodeAnalysisSeven/HashMapTest.java
+++ src/main/java/com/CollectionCodeAnalysisSeven/HashMapTest.java
@@ -1,17 +1,28 @@
 package com.CollectionCodeAnalysisSeven;
 
 import java.util.HashMap;
-import java.util.Hashtable;
+import java.util.Stack;
 
 public class HashMapTest {
 
     public static void main(String... args) {
 
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        //　是否为空
+        /*HashMap<String, String> stringStringHashMap = new HashMap<>();
         stringStringHashMap.put(null, null);
 
         Hashtable<String, String> stringStringHashtable = new Hashtable<>();
-        stringStringHashtable.put(null, "12");
+        stringStringHashtable.put(null, "12");*/
+
+
+        HashMap<String, String> firstMap = new HashMap<>();
+        firstMap.put("12", "12");
+        firstMap.put("13", "13");
+        HashMap<String, String> secondMap = new HashMap<>(firstMap);
+        System.out.println(secondMap.get("12"));
+
+        System.out.println(3&1569);//1
+        Stack<Integer>[] ss = new Stack[2];
 
     }
 }
---
+++ src/main/java/com/leetcode/domain/maxPointsC.java
@@ -0,0 +1,75 @@
+package com.leetcode.domain;
+
+import java.util.HashMap;
+
+public class maxPointsC {
+
+    public int maxPoints(Point[] points) {
+
+        if(points.length<=2) return points.length;
+
+        int max = 0;
+
+        for (int i = 0; i < points.length; i++) {
+
+            HashMap<Float, Integer> floatIntegerHashMap = new HashMap<>();
+
+            Point startPoint = points[i];
+            int chuzhi=0;
+            int chonghe = 0;
+            for (int j = 0; j < points.length; j++) {
+                if(j==i) continue;
+                Point endPoint = points[j];
+                if (i != j && endPoint.x == startPoint.x && endPoint.y == startPoint.y) {
+                    chonghe++;
+                    continue;
+                }
+                if (endPoint.x == startPoint.x && endPoint.y != startPoint.y) {
+                    chuzhi++;
+                    continue;
+                }
+                float dist = (float) ((endPoint.y - startPoint.y) * 1.0f / (endPoint.x - startPoint.x));
+                if (floatIntegerHashMap.get(dist) == null) {
+                    floatIntegerHashMap.put(dist, 1);
+                } else {
+
+                    floatIntegerHashMap.put(dist, floatIntegerHashMap.get(dist) + 1);
+                }
+
+            }
+
+            int tempt =chuzhi;
+            for (Float sf : floatIntegerHashMap.keySet()) {
+                int integer = floatIntegerHashMap.get(sf);
+                tempt = tempt > integer ? tempt : integer;
+            }
+            max = max > tempt + chonghe  + 1 ? max : tempt + chonghe  + 1;
+
+        }
+        return max;
+
+    }
+
+
+    public static void main(String... args) {
+
+        int i = new maxPointsC().maxPoints(new Point[]{new Point(0, 0), new Point(1, 1), new Point(1 ,- 1)});
+        System.out.println(i);
+
+    }
+
+
+   static class Point {
+        int x;
+        int y;
+        Point() {
+            x = 0;
+            y = 0;
+        }
+
+        Point(int a, int b) {
+            x = a;
+            y = b;
+        }
+    }
+}
